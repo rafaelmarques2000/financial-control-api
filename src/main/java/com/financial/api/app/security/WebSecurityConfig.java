8cc5dd1597fde76de23b60249cc7e9647e6f5ac8
Rafael Marques <rafael@dev.localdomain>
2022-06-23 00:50:34 -0300

    habilita bloqueio rotas

--- src/main/java/com/financial/api/app/security/WebSecurityConfig.java
+++ src/main/java/com/financial/api/app/security/WebSecurityConfig.java
@@ -28,7 +28,7 @@ public class WebSecurityConfig {
 
     @Bean
     SecurityWebFilterChain springWebFilterChain(ServerHttpSecurity http) {
-        String[] patterns = new String[] {"/api/v1/**"};
+        String[] patterns = new String[] {"/api/v1/auth/**"};
         return http.cors().disable()
                 .exceptionHandling()
                 .authenticationEntryPoint((swe, e) -> Mono.fromRunnable(() -> {
